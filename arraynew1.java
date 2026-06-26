4fc4212873f99c37bb49cd257f000effe4709b5c
Santhanayaki <santhanayaki1909@gmail.com>
2026-06-27 04:13:27 +0530

    Add Main class with array initialization and printing

---
+++ arraynew1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Declare and initialize the array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print array elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
